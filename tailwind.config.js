8e832fc7dbf412d18a15c4c7d13b9b96d6b66af2
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-04-17 16:40:46 +0200

    changed font to Arial Bold

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [],
+	theme: {
+		extend: {
+			fontFamily: {
+				// add a font arial-bold that is in public/fonts/arial-bold.ttf
+				"arial-bold": ["Arial-Bold"],
+			},
+		},
+	},
+	plugins: [],
+};
